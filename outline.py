022042921636ed8b34e14a2160582225385aa4a9
Billy Poggi <billy.poggi@gmail.com>
2017-04-11 19:59:33 -0400

    intial commit :)

---
+++ outline.py
@@ -0,0 +1,17 @@
+# get directory
+# scan files
+# scan files for 
+
+# then list the files
+
+# count the files
+
+
+
+
+
+# final result 
+# number of files
+# number fo files per date
+
+# [date] [number of files] [total size of files]
